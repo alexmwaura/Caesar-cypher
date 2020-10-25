592df953a19bf39907efabadc4d1965bcc3a26f6
alexmwaura <alexmwaura43@gmail.com>
2020-10-25 17:26:48 +0300

    add test case for indexaArray method

--- src/test/java/CaesarCypherTest.java
+++ src/test/java/CaesarCypherTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class CaesarCypherTest {
@@ -7,6 +9,15 @@ public class CaesarCypherTest {
     public void getIndex_returnTheIndexOfASingleLetter_0(){
         CaesarCypher caesarCypher = new CaesarCypher();
         int index = 0;
-        assertEquals(index,caesarCypher.getIndex("a"));
+        assertEquals(index,caesarCypher.getIndex('a'));
+    }
+
+    @Test
+    public void indexArray_returnIndexOfWord_ArrayList(){
+        CaesarCypher caesarCypher = new CaesarCypher();
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+        arrayList.add(0);
+        arrayList.add(1);
+        assertEquals(arrayList,caesarCypher.indexArray("ab"));
     }
 }
