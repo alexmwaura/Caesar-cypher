d6124f94f027bd3e9adb15dec7383c3e264117a0
alexmwaura <alexmwaura43@gmail.com>
2020-10-25 19:59:10 +0300

    add test cases for encrypt

--- src/test/java/CaesarCypherTest.java
+++ src/test/java/CaesarCypherTest.java
@@ -1,23 +1,38 @@
 import org.junit.Test;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class CaesarCypherTest {
     @Test
-    public void getIndex_returnTheIndexOfASingleLetter_0(){
+    public void getIndexLower_returnTheIndexOfASingleLetterLower_66(){
         CaesarCypher caesarCypher = new CaesarCypher();
-        int index = 0;
-        assertEquals(index,caesarCypher.getIndex('a'));
+        Integer index = 66;
+        assertEquals(index,caesarCypher.getIndex('b'));
     }
 
     @Test
     public void indexArray_returnIndexOfWord_ArrayList(){
         CaesarCypher caesarCypher = new CaesarCypher();
         ArrayList<Object> arrayList = new ArrayList<Object>();
-        arrayList.add(0);
-        arrayList.add(1);
-        assertEquals(arrayList,caesarCypher.indexArray("ab"));
+        arrayList.add(65);
+        arrayList.add(98);
+        assertEquals(arrayList,caesarCypher.indexArray("aB"));
+    }
+    @Test
+    public void indexArray_detectSpace_ArrayList(){
+        CaesarCypher caesarCypher = new CaesarCypher();
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+        arrayList.add(65);
+        arrayList.add(-1);
+        assertEquals(arrayList, caesarCypher.indexArray("a "));
+    }
+    @Test
+    public void encryptedString_encryptString_String(){
+        CaesarCypher caesarCypher = new CaesarCypher();
+        String encryptedString = "mjqqt";
+        assertEquals(encryptedString,caesarCypher.encryptDecrypt("hello",5, "encrypt"));
     }
 }
