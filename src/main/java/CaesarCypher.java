9447ada0f18bf526d1053efa3893fe8fe9c7e28b
alexmwaura <alexmwaura43@gmail.com>
2020-10-25 17:26:12 +0300

    add method to save index of character

--- src/main/java/CaesarCypher.java
+++ src/main/java/CaesarCypher.java
@@ -2,8 +2,15 @@ import java.util.ArrayList;
 
 public class CaesarCypher {
     private String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public Object getIndex(String word){
-        int index = alphabets.toLowerCase().indexOf(word);
+    public Object getIndex(char letter){
+        int index = alphabets.toLowerCase().indexOf(letter);
         return index;
+    }
+    public Object indexArray(String word){
+        ArrayList<Object> integerIndex = new ArrayList<Object>();
+        for(char letter: word.toCharArray()){
+            integerIndex.add(getIndex(letter));
+        }
+        return integerIndex;
     }
 }
